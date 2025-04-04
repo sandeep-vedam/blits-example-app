b35006036c9105997614c88cfc98589ac6d94ec7
sandeep-vedam <sandeepsharma.vedam@outlook.com>
2025-04-04 18:07:18 +0530

    Removed console logs

--- tests/backstopjs/engine_scripts/right-keypress.cjs
+++ tests/backstopjs/engine_scripts/right-keypress.cjs
@@ -1,8 +1,6 @@
 module.exports = async (page, scenario, args) => {
     console.log('SCENARIO > ' + scenario.label)
-    console.log('SCENARIOS-------',scenario.scriptArgs!== undefined ? scenario.scriptArgs[0].value : 0 )
     let count = scenario.scriptArgs !== undefined ? scenario.scriptArgs[0].value : 0
-    console.log('SCRIPT ARGS:', count)
     console.log('Waiting for 1 seconds before pressing the right arrow key...')
     await new Promise((resolve) => setTimeout(resolve, 1000))
     for(let i = 0; i < count;i++ ){
